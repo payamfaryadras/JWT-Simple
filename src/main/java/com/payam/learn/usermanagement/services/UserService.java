2f6f023872b0587989fd391f37b4ae8a4c3105b1
payam <payam.ff@gmail.com>
2020-01-16 18:34:54 +0330

    add the user service

--- src/main/java/com/payam/learn/usermanagement/services/UserService.java
+++ src/main/java/com/payam/learn/usermanagement/services/UserService.java
@@ -1,14 +1,20 @@
 package com.payam.learn.usermanagement.services;
 
 import com.payam.learn.usermanagement.models.User;
+import com.payam.learn.usermanagement.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
 
-    public User createUser(User user){
-        return null;
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public User createUser(User user) {
+        return userRepository.save(user);
     }
 
 
