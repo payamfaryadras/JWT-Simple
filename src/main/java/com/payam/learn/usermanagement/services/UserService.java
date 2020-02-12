f1e6846262e08899f000d19b9bdcb10c2af7a11c
payam <payam.ff@gmail.com>
2020-02-12 12:15:22 +0330

    fixed some  validation problems

--- src/main/java/com/payam/learn/usermanagement/services/UserService.java
+++ src/main/java/com/payam/learn/usermanagement/services/UserService.java
@@ -15,14 +15,14 @@ public class UserService {
 
 
     public User createUser(User user) {
-        if ((user.getPassword() != null || !user.getPassword().isBlank()) &&
-               (user.getEmail() != null || !user.getEmail().isBlank())) {
+        if ((user.getPassword() == null || user.getPassword().isBlank()) ||
+                (user.getEmail() == null || user.getEmail().isBlank())) {
+            throw new CustomException.IllegalEntityStateException("email and pass cannot be blank.");
+        } else {
             if (userRepository.findByEmail(user.getEmail()).isPresent()) {
 //TODO
                 throw new CustomException.DuplicateEntityException("the user already exist.");
             }
-        } else {
-            throw new CustomException.IllegalEntityStateException("email and pass cannot be blank.");
         }
 
         return userRepository.save(user);
